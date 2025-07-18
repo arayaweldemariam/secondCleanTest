0b1e74b6c8b5cf1c46a9672365a6ddfdb926d3c7
araya <arieman40@gmail.com>
2025-07-18 15:05:45 +0300

    Anynomouse first commit

---
+++ AnynomouseClass/src/com/Inner/InnerClass.java
@@ -0,0 +1,34 @@
+package com.Inner;
+
+		interface greeting
+		{
+		  public void show();	
+		}
+
+class greet
+{
+public void gret(greeting g)
+		{
+		 System.out.println("this  inner class");	
+		}
+}
+
+public class InnerClass 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		greet g=new greet();
+		g.gret(new greeting()
+				{
+             		@Override
+					public void show() 
+					{
+						System.out.println("this is from inner class");
+					}
+			
+				});
+	}
+
+}
